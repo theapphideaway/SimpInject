024bf09399d38a2e8495e6d0533b7229ea1eceb1
Ian Schoenrock <32747586+theapphideaway@users.noreply.github.com>
2022-10-18 11:08:12 -0600

    Initial Commit

---
+++ SimpInject/SimpInjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpInjectApp.swift
+//  SimpInject
+//
+//  Created by ian schoenrock on 10/18/22.
+//
+
+import SwiftUI
+
+@main
+struct SimpInjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
